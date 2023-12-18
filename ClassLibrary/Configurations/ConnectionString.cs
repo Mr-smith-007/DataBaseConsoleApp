fb04227abc50aadf674aba62d7897b590fac1241
Repnikov.A <repnikov.a@florcom.ru>
2023-12-18 12:04:16 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary/Configurations/ConnectionString.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Identity.Client;
+
+namespace ClassLibrary
+{
+    public static class ConnectionString
+    {
+        public static string MsSqlConnection => @"Server=.\RepnikovA\SQLEXPRESS;Database=testing;Trusted_Connection=True;";  
+    }
+}
---
+++ ClassLibrary/MainConnector.cs
@@ -0,0 +1,35 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace ClassLibrary
+{
+    public class MainConnector
+    {
+        private SqlConnection connection;
+
+        public async Task<bool> ConnectAsync()
+        {
+            bool result;
+            try
+            {
+                connection = new SqlConnection(ConnectionString.MsSqlConnection);
+                await connection.OpenAsync();
+                result = true;
+            }
+            catch
+            {
+                result = false;
+            }
+
+            return result;
+        }
+
+        public async void DisconnectAsync()
+        {
+            if (connection.State == ConnectionState.Open)
+            {
+                await connection.CloseAsync();
+            }
+        }
+    }
+}
